672dbb977730bfe802a3b3747cc4ae1bfd9e02c2
Timothy Ivins <43072347+TimothyIvins@users.noreply.github.com>
2018-12-10 14:30:42 -0500

    Adding Day 2

---
+++ Day-02/script.js
@@ -0,0 +1,320 @@
+const inputIDs = `ayitmcjvlhedbsyoqfzukjpxwt
+agirmcjvlheybsyogfzuknpxxt
+wgirmcjvlvedbsyoqfzujnpxwt
+agizmcjvlhedbsyoqfzuenlxwt
+aryrmcjvlheebsyoqfzuknpxwt
+agirmcjelhedbsyoqfzukosxwt
+azirmcjvlhedbsooqfzuknpxvt
+agirmcjvffedbsyoqfzudnpxwt
+agilmcjvlhedbsyrqfzuknpxrt
+agirmcjvlhndbsyoofzukcpxwt
+awirmcjvlhedbsyoqfzuknpxlz
+aghrmcjmlhewbsyoqfzuknpxwt
+apirmcjvlmedbsyoqfzcknpxwt
+jgiricjvlhedbsyrqfzuknpxwt
+abirmcjvlbedbsyoqfzuknpxwo
+agirmcjvlhedbsyojfzuknpgkt
+agicmclvlhedbmyoqfzuknpxwt
+aslrzcjvlhedbsyoqfzuknpxwt
+agiqmcjvlhedbsymqfzurnpxwt
+agirmcjvlwedbsyoqfzuknfxmt
+agiumcjvlhedbsyoqfzuknpbyt
+xgirxcjvlwedbsyoqfzuknpxwt
+bgtrvcjvlhedbsyoqfzuknpxwt
+afirmcjvlpedbsyoqvzuknpxwt
+agirmcjjvhedbsyoqfzukmpxwt
+ggirmcjvlhedbsyoqfzukypxvt
+agirmdjulhekbsyoqfzuknpxwt
+agirmcjqlhedbsyoqfztknixwt
+agirmcjvjhedbsyomfduknpxwt
+agirmcjvlhedbgyoqfzuknpxtq
+agirmvjvlhbdbsyfqfzuknpxwt
+agirmcjvlhedbsyatfzbknpxwt
+agirmcjvlrlybsyoqfzuknpxwt
+agirmajvlhedbsqovfzuknpxwt
+abinmcrvlhedbsyoqfzuknpxwt
+agnrmcjvlhedbsyurfzuknpxwt
+agirmpjvlhedbsyoqezuknpxct
+agirmxjvlhedbsgoqjzuknpxwt
+agirmcjvlhehbstoqfzuknpxht
+qgirmcjvlhepcsyoqfzuknpxwt
+tgirmcjvlhkdbsyoqszuknpxwt
+agirmcjvdhedbscoqftuknpxwt
+agbrmcjvlhedbsyoqfzukqpxwj
+agurmcjvlhedbsyaqfzuknpxmt
+agirmcjvohudbsyoqfmuknpxwt
+agirmcjvlhekbsyoqfbuktpxwt
+agirmcjvlhedhsyoqfzugnnxwt
+agirmcjvlhedbsyjqyzuknpxft
+agirmcjvlhedbsymufznknpxwt
+agirmcjhlheubsyoqfzuknpxmt
+agirmcjvlhwdbsywqfzwknpxwt
+agirmcjvljedbsgqqfzuknpxwt
+aglrmcjelhedbsyoqfzuknpxkt
+agxrmcjvlhxdbsyoqfquknpxwt
+agirmcjvnhedbsyoqfzuenfxwt
+agirmcjvlhedbsyoqfzatnqxwt
+agirmcvvlhedbsboqfzuknuxwt
+agirncjvlhezbsyoqfzulnpxwt
+agiamcjvdiedbsyoqfzuknpxwt
+agirmcjvwhedbskoqfzhknpxwt
+agiwmcjflhedbsyoqfzulnpxwt
+agirmcjvlhedboyoqfzuknpjwl
+agivmcjslhedbsyoqfzdknpxwt
+agirmcjvlcedbsyoqfzukepxyt
+akirmcjvlhjdbssoqfzuknpxwt
+agvrmcjvldedmsyoqfzuknpxwt
+agirecjvlhidbsyoqfzukbpxwt
+abirmcjvlhjdbsyoqfkuknpxwt
+agirmcjelhedbfyoqfzuknpxwj
+agirmcjvlhedbbyoqrzukwpxwt
+akirmcjvlhedbsyoyfzuknplwt
+agirmcjvlhedbsydsfzuknpxwq
+agirrcjvlhedbsyoqazuknpmwt
+aeirmcjvlhedbsyoqfvuknpwwt
+akirmcjvlhedbsyoqpzudnpxwt
+agijmcjvlhedbsyuqfzunnpxwt
+agirmcjilhedasyoqizuknpxwt
+agirmczvlhzdbsyoqfzuknpxwx
+agirmcjvlhehbsyoifzuknpxwo
+agirwcjvlhedbsyoqfzuenpxst
+agirmcjvlhedbsyoquzuknhxft
+agirmcqvlkedbsyoqfzrknpxwt
+agirmcqvlhenbsyoqfzuknpuwt
+agirmcjvleedbsyoqfzhhnpxwt
+agirmcjvlhembsyrqfauknpxwt
+agirmcjvlhedbssoqflcknpxwt
+aqirmcjvlnedbsyoqfzuknpxpt
+agirmcjqlhedbxpoqfzuknpxwt
+fgirmcjvlhedbsyoqfzukqpqwt
+aggrmcjvlhpdbsyoqfzuknpxjt
+agirmwjvlhedbsywqfzuknpzwt
+agirmcailhembsyoqfzuknpxwt
+aglrmcjvlhxdbsyoqfzuknpxet
+xgirmcjvlhzdbsyoqfzukrpxwt
+agvrmcjvuhedbsyoqfzuknpxgt
+agikmcjvlhecbsyoqfzuknpxwr
+agyrmcjvlhezbsyoqfouknpxwt
+agirmcjvfhjdbsyokfzuknpxwt
+agkrmjjvlhedtsyoqfzuknpxwt
+agirmgjvlhedbiyoqfzuknpxwv
+wcirmcjvlhedbsyoqfzuknpxwo
+aairmcjvlhedbstoqfguknpxwt
+hgirmcjvlhedwfyoqfzuknpxwt
+agirmcjvmhfdbmyoqfzuknpxwt
+agirmcjvlhvdbsioqfzuanpxwt
+agrrmcjvgsedbsyoqfzuknpxwt
+agirmcjvlqetbsysqfzuknpxwt
+agirccjvlhedbsyoqfzuknkcwt
+agirmqjvlhedbsdoqfzkknpxwt
+agirmcjvlheobsyopfzuknpxwg
+agirmcjolhedbsyofpzuknpxwt
+agirmcjnlhedbsyoqkzukfpxwt
+agiumcjvlheabsyoqfzuknpxbt
+agipmcjvlhedbsyoqfzukupxwz
+atirmcrvlhedbsyoqfnuknpxwt
+agirmcjvnhedfkyoqfzuknpxwt
+agirmrjvlhedboyoqfzvknpxwt
+abhrmcjvlhedbtyoqfzuknpxwt
+cbirmcjvlhedbfyoqfzuknpxwt
+agirmcjvlhedbsyoqfmwknjxwt
+ahirmcjvlhedbsloqfzuknpfwt
+agarmjjvlhedbsyoqfzyknpxwt
+ajirmcjvlhevjsyoqfzuknpxwt
+agirmcjvlhpdbstoqfzuknpewt
+agirmcsvlhedbsyoqfbupnpxwt
+agirmcjvlhexbsyodfzukqpxwt
+auiymcjblhedbsyoqfzuknpxwt
+azirmcjvchedbsyoqfziknpxwt
+aeirmcjvlhedvsyoqfzuonpxwt
+agirmcjvlhedbfyoqfbukjpxwt
+ygirmcjvlhidbsyoqfzukncxwt
+agirmxpvlhedbsyoqffuknpxwt
+ztirmcjvlhedosyoqfzuknpxwt
+agirmcjvlhepbsyoqfzuenppwt
+agirmcjvshedbsyoqnzaknpxwt
+awirmcjvlhydbsyoqfzuknoxwt
+ucirmcjvlhedbsyoqfjuknpxwt
+agirmwjvlhkbbsyoqfzuknpxwt
+agirmcjvldedbsyohfzuknpxzt
+agirmcjvwhedbsyoqfznknpxgt
+agiricjvlhedxqyoqfzuknpxwt
+agirmcjvlhzdbjyoqfzukapxwt
+agirmcgvlhedbsyoqfzuknaowt
+agidmcjvlhedbsyoqayuknpxwt
+agirmcjvlhedisnoqfzuknpxnt
+wkjrmcjvlhedbsyoqfzuknpxwt
+agirmcjvlhedbuyojfzukxpxwt
+agkrmcjvlhedbsybqfzurnpxwt
+agirmcjvghedbsyoqfzuknexwj
+agirmcjvnhedbsyoqfzuznpxit
+agirmcjvlbedbsyoqfiukwpxwt
+agirlctvlheabsyoqfzuknpxwt
+agirmcjzzhedbsyoqfzcknpxwt
+akirmcjvlnedbsyoqfzlknpxwt
+agirmdjvlhedpsyoqfzuknpjwt
+agiyjcuvlhedbsyoqfzuknpxwt
+agirmcbvltedysyoqfzuknpxwt
+agirmcjvlhedfdyoqfzubnpxwt
+agidmcjvlhedesfoqfzuknpxwt
+aeirmcjvlhedqsyoqfxuknpxwt
+agifmcjvlhedbsyoqfquknptwt
+agidmcjvlhedbsyfqfzuknpxwb
+agirvcjvlhedbsroqfzuknjxwt
+agirmcqvlhddbsyoqfzuknpxwj
+agirmcjvlhmdqsyoqizuknpxwt
+atirmcjvltedbsyoqfzuknpxwz
+agirxnjvlhedbsyoqfzuknpxkt
+agihmcjvlhedbsyoqfzukepxqt
+agirmcjvlhedbsmoqzsuknpxwt
+agirycjvlhedbuyoqfwuknpxwt
+agirmcjvlhedbsyoqfzfkrfxwt
+agirzcjvlhedbsyoqfhuknpxnt
+agigmcjvlhedbsqnqfzuknpxwt
+agirmgzvlhedbsyoqfzuonpxwt
+agirmcjvqhedbqyoqfzukqpxwt
+anarmcjvlhedbsyocfzuknpxwt
+agirmcjuihedbshoqfzuknpxwt
+agirdckvlhedbsyoqfzxknpxwt
+ugirmujvlhwdbsyoqfzuknpxwt
+mgirmcjvlheobsyovfzuknpxwt
+agirmcjvghedbsyoqfzufxpxwt
+agirmcjvlhedbsyoinzuknuxwt
+agirmzjvlhbdbsyoqfzlknpxwt
+agivmcjvlhedbsconfzuknpxwt
+agirmwfvlhedtsyoqfzuknpxwt
+agirmcjvlhedbbyoqrzukncxwt
+agirmcjvlhelbsyoqfzupnlxwt
+agirmmjvluedqsyoqfzuknpxwt
+agjrmcjvlhedbsyaqfcuknpxwt
+agiwmcjvlhedbsyoqzzuknpswt
+agirxcjvlhedbsyoqfyvknpxwt
+agirmljvlhedbsyoqkzuknpxjt
+agirmcjvchedbsyoqfzmknyxwt
+agirmcjvlhedbsyovfzuynpxwl
+agtrmcjvlhedysyoqfzuknexwt
+agirmcjvmhedbslonfzuknpxwt
+agirmcjfshedbsyoqfziknpxwt
+agirmcjvlhedbsygqfzkknpbwt
+agyrmcivlhedbsyovfzuknpxwt
+agirmcjvghedbsyoqjzuknkxwt
+agirmcjvlhedqsyoqfzukspxmt
+ayirmcjvhhedbsyomfzuknpxwt
+agirmcjvlnembsypqfzuknpxwt
+agirmcjqlhedbsyuvfzuknpxwt
+agirmcjvlhembsybqfzuknpxwa
+agirjcfvlhedbsyoqfuuknpxwt
+agirmcjvohedbsyowfzuknxxwt
+agirmcjvlhedroyoqfzukncxwt
+agrrmijvlhedbsyoqfnuknpxwt
+agirmjjvlhsdbsyoqfzumnpxwt
+agirrcjvnhedbsyoqfzuktpxwt
+agirmcjvlzedjsyoqfzuknpdwt
+agirmkjvlhedbsyoqfzxinpxwt
+agirmcjvlhedbzyojfzuknpvwt
+arirmcjvlheddsyoqfzuknrxwt
+agirmcjvlhedbsyoqhzuanpxmt
+agirmcjvluedbsyoqozuknwxwt
+afirmcjwlhedxsyoqfzuknpxwt
+agirmcjvlhefbsyoqfkuinpxwt
+agirycjvltedbsypqfzuknpxwt
+agirmrxvlhedbsyoqfzeknpxwt
+agfrmcqvlhedbsyoqxzuknpxwt
+agormcjvuhexbsyoqfzuknpxwt
+agyrmcjvehddbsyoqfzuknpxwt
+agirmcjvlheqbsynqfzgknpxwt
+agirmcjvlhedbsloufwuknpxwt
+tgirmcjvlwedbsyoqfzuknpqwt
+agirmcjvlhesbzyogfzuknpxwt
+agitmdjvlhedpsyoqfzuknpjwt
+bgirmejvlhtdbsyoqfzuknpxwt
+aginmcjvlhedzsyoqfzuknoxwt
+agvrzcjvlhedbsuoqfzuknpxwt
+agormcjvlhedbsyoqfzuknpodt
+agirmcevlhedbgyojfzuknpxwt
+agirmcjblhedboytqfzuknpxwt
+qgibmcjvlhedbsyoqfzuknbxwt
+agirmcjvlhedbsyoafzutnnxwt
+agiamcjvchkdbsyoqfzuknpxwt
+agirmcjvehedblyoqwzuknpxwt
+agirmcpvlhwdbsyoafzuknpxwt
+agirmcjvlhtdbsyoqfzumnpxtt
+agirmcjalhegtsyoqfzuknpxwt
+agirdijvlhedbsyoqfzutnpxwt
+agirmckvlhgdbsyovfzuknpxwt
+qgmrmcjvlkedbsyoqfzuknpxwt
+agirjcjvlhodbsyoqfzuanpxwt
+ajirmcjvlhedbpyoqftuknpxwt
+cgirmcjvlhedbsyoqfiuonpxwt
+ayirmcjvlhedbsyaqfzuknwxwt
+agirmcjvlhedbdyoqbzwknpxwt`
+
+const arrIDs = inputIDs.split("\n");
+
+// Part 1
+
+const exactNumOfChars = (inString, numChars) => {
+  let count;
+  let i;
+  let j;
+  for (i = 0; i < inString.length; i++) {
+    count = 0;
+    for (j = 0; j < inString.length; j++) {
+      if ((i !== j) && (inString.charAt(i) === inString.charAt(j))) {
+        count++;
+      }
+    };
+    if (count === numChars - 1) {
+      return true;
+    };
+  };
+  return false;
+};
+
+let twoCount = 0;
+let threeCount = 0; 
+
+arrIDs.forEach(inStr => {
+  if (exactNumOfChars(inStr, 2)) {
+    twoCount++;
+  };
+  if (exactNumOfChars(inStr, 3)) {
+    threeCount++;
+  };
+});
+
+console.log("Part 1 checksum", twoCount * threeCount);
+
+// Part 2
+
+const findCommonLetters = () => {
+  let i;
+  let j;
+  let k;
+  let diffCharIndex;
+  loopi:
+  for (i = 0; i < arrIDs.length - 1; i++) {
+    loopj:
+    for (j = i + 1; j < arrIDs.length; j++) {
+      if (arrIDs[i].length === arrIDs[j].length) {
+        count = 0;
+        loopk:
+        for (k = 0; k < arrIDs[i].length; k++) {
+          if (arrIDs[i].charAt(k) !== arrIDs[j].charAt(k)) {
+            count++;
+            if (count > 1) {
+              break loopk;
+            };
+            diffCharIndex = k;
+          };
+        };
+        if (count === 1) {
+          return arrIDs[i].slice(0, diffCharIndex) + arrIDs[i].slice(diffCharIndex + 1);
+        };
+      };
+    };
+  };
+};
+
+console.log("Part 2 common letters", findCommonLetters());
+
